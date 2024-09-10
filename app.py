911f84fb85256bd30a30e6e78bb3f051b9db6fb7
이원빈 <2wonbin@kakao.com>
2024-09-10 22:36:47 +0900

    app 진입 페이지 설정 및 마크다운 추가

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 import streamlit as st
+
+st.set_page_config(
+    page_title="FullstackGPT Home",
+    page_icon="🤖",
+)
+
+st.markdown(
+    """
+    # Hello!
+            
+    Welcome to my FullstackGPT with Nomad Coders! 
+    """
+)
